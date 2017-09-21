13e4747a485ae2f7dc5020d2d1dcb9b3673bb0d9
Tony Fujs <tony.fujs@gmail.com>
2017-09-21 16:30:38 -0400

    add update_node()

---
+++ R/update_node.R
@@ -0,0 +1,21 @@
+#' update_node
+#'
+#' Update an exisiting DAKN node
+#'
+#' @param nid character: DKAN node ID
+#' @param url character: The DKAN site URL
+#' @param body json: A JSON object to be passed to the body of the request
+#' @param as character: Output format. Options are: 'json' or 'list'
+#' @param ... Other optional parameters passed to the underlying GET request
+#'
+#' @return dkan_node object
+#' @export
+#'
+#' @examples
+#' update_node(nid = 1, url = "http://demo.getdkan.com")
+update_node <- function(nid, url = get_url(), body, as = 'json', ...) {
+  # CHECK: input validity
+  assertthat::assert_that(!is.null(nid), msg = 'The Node ID (nid) argument must be specified')
+  res <- dkan_PUT(url = url, nid = nid, body, ...)
+  switch(as, json = res, list = as_dk(jsl(res), "dkan_node"))
+}
