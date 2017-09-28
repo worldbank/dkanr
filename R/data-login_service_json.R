8b4cb5f6cd83fb830c55df2c1cadccbf4719ab72
Tony Fujs <tony.fujs@gmail.com>
2017-09-28 10:36:36 -0400

    add data documentation

---
+++ R/data-login_service_json.R
@@ -0,0 +1,8 @@
+#' JSON body for login service request
+#'
+#'
+#' @format A list of 3.
+#'
+#' @examples
+#' login_service_json
+"login_service_json"
---
+++ R/data-system_connect_json.R
@@ -0,0 +1,8 @@
+#' JSON body for system connect request
+#'
+#'
+#' @format A list of 3.
+#'
+#' @examples
+#' system_connect_json
+"system_connect_json"
