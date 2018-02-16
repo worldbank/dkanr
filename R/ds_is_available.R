c2b7d7fefa21489bc5e3b4137d13ead7cb4d9072
Tony Fujs <tony.fujs@gmail.com>
2018-02-16 15:12:01 -0500

    add ds_is_available() helper function

---
+++ R/ds_is_available.R
@@ -0,0 +1,24 @@
+#' ds_is_available
+#'
+#' Returns TRUE is a data file has been imported in the DKAN datastore, returns FALSE otherwise
+#'
+#' @param metadata list: Resource level metadata
+#'
+#' @return logical vector
+#' @export
+#'
+
+ds_is_available <- function(metadata) {
+
+  # CHECK input
+  assertthat::assert_that(is.list(metadata) == TRUE,
+                          msg = 'metadata must be a list')
+  assertthat::assert_that(metadata$type == "resource",
+                          msg = 'This node is not of type "resource". Resource links are only available in resource nodes')
+
+  out <- if (metadata[['field_datastore_status']][['und']][[1]][['value']] == "1") {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
