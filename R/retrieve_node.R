b55f06b38f38620e5eb4c757ab812d26fd8db436
Tony Fujs <tony.fujs@gmail.com>
2018-02-20 13:29:41 -0500

    don't run examples with API calls

--- R/retrieve_node.R
+++ R/retrieve_node.R
@@ -12,7 +12,9 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' retrieve_node(nid = 1, url = "http://demo.getdkan.com")
+#' }
 retrieve_node <- function(nid, url = get_url(), credentials = list(cookie = dkanr::get_cookie(), token = dkanr::get_token()), as = "json", ...) {
   # CHECK: input validity
   assertthat::assert_that(
