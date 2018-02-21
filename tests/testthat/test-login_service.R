fe33ce5c28118a3d81c7c7401271ff3b982776d4
Tony Fujs <tony.fujs@gmail.com>
2018-02-21 17:38:19 -0500

    fix login service test

--- tests/testthat/test-login_service.R
+++ tests/testthat/test-login_service.R
@@ -5,7 +5,7 @@ dkanr_setup(url = 'https://datacatalog.worldbank.org/')
 
 # start_capturing(path = './tests/testthat')
 # login_service(root_url = get_url(),
-#               system_connect_sessid = connect_system(root_url = get_url()),
+#               system_connect_sessid = "Ea9b4qskV0cAxyYTO9WOVTGdLlz66wXn2U7p10eiX6I",
 #               username = Sys.getenv("ddh_username"),
 #               password = Sys.getenv("ddh_prod_password"))
 # stop_capturing()
@@ -13,7 +13,7 @@ dkanr_setup(url = 'https://datacatalog.worldbank.org/')
 httptest::with_mock_api({
   test_that("Login service is working as expected", {
     resp <- login_service(root_url = get_url(),
-                          system_connect_sessid = connect_system(root_url = get_url()),
+                          system_connect_sessid = "Ea9b4qskV0cAxyYTO9WOVTGdLlz66wXn2U7p10eiX6I",
                           username = Sys.getenv("ddh_username"),
                           password = Sys.getenv("ddh_prod_password"))
     expect_is(resp, "character")
