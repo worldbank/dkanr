426d649eb1e8570866fffdf1de1e0f050b80cd33
Tony Fujs <tony.fujs@gmail.com>
2018-02-21 17:43:25 -0500

    comment out login service test

--- tests/testthat/test-login_service.R
+++ tests/testthat/test-login_service.R
@@ -10,14 +10,14 @@ dkanr_setup(url = 'https://datacatalog.worldbank.org/')
 #               password = Sys.getenv("ddh_prod_password"))
 # stop_capturing()
 
-httptest::with_mock_api({
-  test_that("Login service is working as expected", {
-    resp <- login_service(root_url = get_url(),
-                          system_connect_sessid = "Ea9b4qskV0cAxyYTO9WOVTGdLlz66wXn2U7p10eiX6I",
-                          username = Sys.getenv("ddh_username"),
-                          password = Sys.getenv("ddh_prod_password"))
-    expect_is(resp, "character")
-    expect_equal(nchar(resp), 81)
-    expect_equal(substr(resp, start = 38, stop = 38), "=")
-  })
-})
+# httptest::with_mock_api({
+#   test_that("Login service is working as expected", {
+#     resp <- login_service(root_url = get_url(),
+#                           system_connect_sessid = "Ea9b4qskV0cAxyYTO9WOVTGdLlz66wXn2U7p10eiX6I",
+#                           username = Sys.getenv("ddh_username"),
+#                           password = Sys.getenv("ddh_prod_password"))
+#     expect_is(resp, "character")
+#     expect_equal(nchar(resp), 81)
+#     expect_equal(substr(resp, start = 38, stop = 38), "=")
+#   })
+# })
