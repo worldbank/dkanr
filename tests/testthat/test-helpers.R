61d4cbf898abeef6fa3569d8687c138db822d80b
Tony Fujs <tony.fujs@gmail.com>
2018-02-23 17:52:17 -0500

    fix typo

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -4,6 +4,6 @@ trail <- "string with trail////"
 
 test_that("helpers functions return expectd values", {
   expect_equal(dk(), "api/dataset/node")
-  expect_equal(dkfiles, "sites//default//files")
+  expect_equal(dkfiles(), "sites//default//files")
   expect_equal(notrail(trail), "string with trail")
 })
