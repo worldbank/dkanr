8cb737eaa56bdb16b6ba5806d2a3e7a8dd786c91
Tony Fujs <tony.fujs@gmail.com>
2018-03-07 11:34:34 -0500

    update test

--- tests/testthat/test-dkanr_settings.R
+++ tests/testthat/test-dkanr_settings.R
@@ -9,8 +9,8 @@ httptest::with_mock_api({
     dkanr_setup(url = 'https://data.louisvilleky.gov/')
 
     expect_equal(get_url(), "https://data.louisvilleky.gov/")
-    # expect_equal(get_cookie(), "SSESSea72a4c978dd1670e2b2d8df72f13a23=mEJC8EiseCDIjWzfkuFCszFQqPnyfDadUfgMuSmtwro")
-    # expect_equal(get_token(), "pc6cosYE12tR4hW-8WspXiNzm1yH1igFMhV2lNU4Zm4")
+    expect_equal(get_cookie(), "")
+    expect_equal(get_token(), "")
 
     expect_true(is.list(dkanr_settings()))
   })
